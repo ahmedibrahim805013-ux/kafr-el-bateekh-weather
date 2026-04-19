1f6c2fb7cd48682fb74d102483d06da433dd42d3
ahmedibrahim805013-ux <ahmedibrahim805013@gmail.com>
2026-04-19 17:07:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,27 @@
-const CACHE_NAME = 'kafr-weather-v1';
+const CACHE_NAME = 'kafr-weather-v2';
 const urlsToCache = [
   '/',
   '/kafr-el-bateekh-weather/',
-  'index.html'
+  'index.html',
+  'manifest.webmanifest'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
   );
 });
